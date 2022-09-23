b3924160804d61a34820f431a67609be02bdc504
marcelacrvalho <marcelacrvalho@icloud.com>
2022-09-23 15:56:29 -0300

    Altera design do app

--- lib/app/modules/home/pages/home_pages.dart
+++ lib/app/modules/home/pages/home_pages.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:reddit_feeling/app/theme/app_theme.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return SafeArea(
+      child: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Stack(
+              children: [
+                Center(
+                  child: Icon(
+                    Icons.bar_chart,
+                    size: Get.mediaQuery.size.width * 0.8,
+                    color: appThemeData.primaryColorLight.withOpacity(0.1),
+                  ),
+                ),
+                Center(
+                  child: Text(
+                    'TICKER',
+                    style: TextStyle(
+                      fontSize: appThemeData.textTheme.titleLarge!.fontSize,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Card(
+              color: appThemeData.primaryColorDark.withOpacity(0.7),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'sentimento',
+                      style: TextStyle(
+                        fontSize: appThemeData.textTheme.bodyMedium!.fontSize,
+                      ),
+                    ),
+                    Text(
+                      'pontuação',
+                      style: TextStyle(
+                        fontSize: appThemeData.textTheme.bodyMedium!.fontSize,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/app/theme/app_theme.dart
+++ lib/app/theme/app_theme.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 
 final ThemeData appThemeData = ThemeData(
-  scaffoldBackgroundColor: Colors.white,
-  primarySwatch: Colors.indigo,
-  primaryColor: const Color(0XFF3f51b5),
-  primaryColorDark: const Color(0XFF303f9f),
-  primaryColorLight: Colors.amber,
+  scaffoldBackgroundColor: Colors.blue.shade900,
+  primarySwatch: Colors.blue,
+  primaryColor: Colors.indigo,
+  primaryColorDark: Colors.blue.shade900,
   dividerColor: Colors.transparent,
-  primaryTextTheme: const TextTheme(
+  primaryColorLight: Colors.white,
+
+  textTheme: const TextTheme(
     titleLarge: TextStyle(
-      color: Colors.black,
-      fontSize: 22.0,
+      color: Colors.white,
+      fontSize: 30.0,
+    ),
+    bodyMedium: TextStyle(
+      color: Colors.white,
+      fontSize: 16.0,
     ),
   ),
 );

acd920d8372d2e44c4306b2a1f2ec3acc96e272e
marcelacrvalho <marcelacrvalho@icloud.com>
2022-09-26 17:31:51 -0300

    Refatora widgets

--- lib/app/modules/home/pages/home_pages.dart
+++ lib/app/modules/home/pages/home_pages.dart
@@ -13,7 +13,8 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         leading: GestureDetector(
-          onTap: () => HomeController.to.isIconClicked.value = !HomeController.to.isIconClicked.value,
+          onTap: () => HomeController.to.isIconClicked.value =
+              !HomeController.to.isIconClicked.value,
           child: Icon(
             Icons.grid_view,
             size: 25.0,
@@ -30,37 +31,11 @@ class HomePage extends StatelessWidget {
   Widget _body() {
     return SafeArea(
       child: Obx(
-          () => !HomeController.to.isIconClicked.value ?
-          Column(
-            children: [
-              Expanded(
-                flex: 2,
-                child: Stack(
-                  children: [
-                    Center(
-                      child: Icon(
-                        Icons.bar_chart,
-                        size: Get.mediaQuery.size.width * 0.8,
-                        color: appThemeData.primaryColorLight.withOpacity(0.1),
-                      ),
-                    ),
-                    Center(
-                      child: Text(
-                        'TICKER',
-                        style: TextStyle(
-                          fontSize: appThemeData.textTheme.titleLarge!.fontSize,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ), const Expanded(child: CustomDataCard())
-            ],
-          ) : const Padding(
-            padding: EdgeInsets.all(3.0),
-            child: CustomDataGridView(),
-          ),
+        () => !HomeController.to.isIconClicked.value
+            ? CustomDataCard(index: HomeController.to.randomIndex,) : const Padding(
+                padding: EdgeInsets.all(3.0),
+                child: CustomDataGridView(),
+              ),
       ),
     );
   }

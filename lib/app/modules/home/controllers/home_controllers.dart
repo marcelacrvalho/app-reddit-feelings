e7f9f40cf4c0581b20b4a608b7201e00fbe81824
marcelacrvalho <marcelacrvalho@icloud.com>
2022-09-26 11:09:41 -0300

    Adiciona regras de negócio

--- lib/app/modules/home/controllers/home_controllers.dart
+++ lib/app/modules/home/controllers/home_controllers.dart
@@ -1,6 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:reddit_feeling/app/data/models/data.dart';
 
 class HomeController extends GetxController {
   static HomeController get to => Get.find<HomeController>();
+  RxList data = [].obs;
+  RxBool isIconClicked = false.obs;
+  List<Color> colorsFeeling = [Colors.red, Colors.green];
 
 }

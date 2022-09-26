f1bae724de016de60c1ab1e3f0fb37a9cae98f3c
marcelacrvalho <marcelacrvalho@icloud.com>
2022-09-26 17:32:30 -0300

    Adiciona comunicação com a API

--- lib/app/data/repositories/data_repositories.dart
+++ lib/app/data/repositories/data_repositories.dart
@@ -4,16 +4,14 @@ import 'package:reddit_feeling/app/data/models/data.dart';
 import 'package:reddit_feeling/app/data/servers/app_servers.dart';
 
 class DataRepository {
-  static Future<List<Data>> fetch() async {
-    var client = http.Client();
-
+  static Future<List<Data>?>? fetch() async {
     try {
-      var response = await http.post(AppServer.uri);
+      var response = await http.get(AppServer.uri);
       var decodedResponse = jsonDecode(response.body) as List;
       List<Data> data = decodedResponse.map((e) => Data.fromJson(e)).toList();
       return data;
-    } finally {
-      client.close();
+    } catch (e){
+      return null;
     }
   }
 }
